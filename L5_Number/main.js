cfed0d27bd24751e496ff46dc26aeddb17330296
Thongchu0203 <thongct@bsscommerce.com>
2021-08-20 20:41:59 +0700

    hello
    aaaaa

---
+++ L5_Number/main.js
@@ -0,0 +1,9 @@
+var age=18 ;
+var Pi =3.14 ;
+var otherNumber=new Number(30); // Không nên sử dụng 
+// To String 
+
+var x= age.toString();
+
+//To Fixec làm tròn đến chữ số thập phân  để làm tròn số thập phân nếu không truyền thì sẽ tự hiểu 
+// là số 0 
